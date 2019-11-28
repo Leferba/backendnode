96a82e6fdd80f5b474311e5272d3dce546461463
Lenin <lenin.bautista@hotmail.com>
2019-11-27 19:54:06 -0500

    coneccion con socket io

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express =require ('express');
+const app = express();
+const server=require('http').Server(app);
+
 const bodyParser = require ('body-parser');
-
+const socket = require('./socket');
 const db = require('./db');
-
 const router = require('./network/routes'); 
 
 db('mongodb+srv://leninbautista:ci-Bf2pKajUxU&M@cluster0-dmwha.mongodb.net/test?retryWrites=true&w=majority');
-var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-// app.use(router);
+
+socket.connect(server);
 
 router(app);
 
-
-
 app.use('/app',express.static('public'));
 
-app.listen(3000);
-console.log('La aplicación está escuchando en http://localhost:3000');
+server.listen(3000, function () {
+  console.log('La aplicación está escuchando en http://localhost:3000');
+});
